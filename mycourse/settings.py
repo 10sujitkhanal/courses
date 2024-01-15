0ab0e2c3eb7a92168ab0208a0ed8423527f82982
10sujitkhanal <65459528+10sujitkhanal@users.noreply.github.com>
2024-01-15 23:04:27 +0545

    Update settings.py

--- mycourse/settings.py
+++ mycourse/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'mycourse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'zqixtwjb', 
+        'USER': 'zqixtwjb',
+        'PASSWORD': 'I8j6UTpxdL98nQU93oS4H1WYSnGmCBSb',
+        'HOST': 'satao.db.elephantsql.com', 
+        'PORT': '5432',
     }
 }
 
